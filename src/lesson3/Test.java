3ea27532900566e7f6da08e936687a532911a6ec
Yurii-Kudlay <yurgen_2014@ukr.net>
2016-03-31 00:39:41 +0300

    test

---
+++ src/lesson3/Test.java
@@ -0,0 +1,11 @@
+package lesson3;
+
+/**
+ * Created by yurge on 31.03.2016.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/lesson3/Test1.java
@@ -0,0 +1,7 @@
+package lesson3;
+
+/**
+ * Created by yurge on 31.03.2016.
+ */
+public class Test1 {
+}
