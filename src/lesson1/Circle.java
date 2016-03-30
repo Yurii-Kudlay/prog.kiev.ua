42b377e031e114e2d4beba164cbb33eda8611f31
Yurii-Kudlay <yurgen_2014@ukr.net>
2016-03-31 00:08:53 +0300

    test

--- src/lesson1/Circle.java
+++ src/lesson1/Circle.java
@@ -1,1 +1,24 @@
-package lesson1;/** * Created by yurge on 29.03.2016. */public class Circle {    private double radius;    public Circle(double radius) {        this.radius = radius;    }    public double getSquare (){        return Math.PI * radius * radius;    }    public static void main(String[] args) {        Circle circle = new Circle(7);        Circle circle2 = new Circle(5);        System.out.println(circle.getSquare());        System.out.println(circle2.getSquare());    }}
+package lesson1;
+
+/**
+ * Created by yurge on 29.03.2016.
+ */
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getSquare (){
+        return Math.PI * radius * radius;
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle(7);
+        Circle circle2 = new Circle(5);
+        System.out.println(circle.getSquare());
+        System.out.println(circle2.getSquare());
+    }
+}
